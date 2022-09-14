895820f36d635b32983f64e33c7eb1b73463827b
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-09-14 20:10:06 +0530

    created action

--- Frontend/src/actions/bookAction.js
+++ Frontend/src/actions/bookAction.js
@@ -1,4 +1,21 @@
-export const getBookDetailSucess = {
-    type: "GET_BOOK_DETAIL",
-    payload: true
-  };
+import axios from "axios";
+import { bookConstant } from "../constants"
+
+export const getBookDetailSucess = (res) => ({
+    type: bookConstant.GET_ALL_BOOK_DETAIL,
+    response: res
+});
+
+export const getAllBooksDetail = () => {
+    return (dispatch) => {
+        axios
+        .get("http://localhost:5000/books/")
+        .then(response => {
+            console.log("-----response-----", response)
+        dispatch(getBookDetailSucess(response?.data))
+    })
+    .catch((err) => {
+        console.log("----error-----", err)
+    })
+    }
+}
