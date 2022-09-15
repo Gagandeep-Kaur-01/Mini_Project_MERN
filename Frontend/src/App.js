6c02725ad33f8ddaa5b148208b55172ecca575c7
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-09-15 11:25:22 +0530

    component calling

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,14 +3,17 @@ import { BooksDetail }  from "./Components/booksDetail";
 import Homepage from "./Components/Homepage/homepage";
 import Login from "./Components/Login/login";
 import Register from "./Components/Register/register";
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <Homepage />
-      <Login />
-      <Register />
-      {/* <BooksDetail /> */}
+      {/*
+        <Login />
+        <Register />
+        <BooksDetail /> 
+      */}
      
     </div>
   );
