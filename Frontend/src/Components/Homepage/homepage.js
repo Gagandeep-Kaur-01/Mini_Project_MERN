35e7a85afc80f3b2d219f1776cb87056c30c786c
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-09-15 10:23:09 +0530

    created folder structure

---
+++ Frontend/src/Components/Homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.css";
+
+const Homepage = () => {
+    return(
+        <div>
+            Homepage
+        </div>
+    )
+}
+
+export default Homepage;
