98dec91bf02380288016608d1d8feeb9b0dc9bfc
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-09-15 11:53:05 +0530

    login page

--- Frontend/src/Components/Login/login.js
+++ Frontend/src/Components/Login/login.js
@@ -3,8 +3,13 @@ import "./login.css";
 
 const Login = () => {
     return(
-        <div>
-            Login
+        <div className="login">
+            <h1>Login</h1>
+            <input type="text" placeholder="Enter your Email"></input>
+            <input type="password" placeholder="Enter your Password"></input>
+            <div className="button">Login</div>
+            <div>or</div>
+            <div className="button">Register</div>
         </div>
     )
 }
