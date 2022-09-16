9f923d628a1e03ffd3b4c87ffb9a829c08cbc0e0
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-09-16 22:09:22 +0530

    login page

--- Frontend/src/Components/Login/login.js
+++ Frontend/src/Components/Login/login.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import "./login.css";
 
 const Login = () => {
+
+    const [ user, setUser] = useState({
+        email: "abc@gmail.com",
+        password: "",
+    })
+
+    const handleChange = e => {        
+        const { name, value } = e.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+
     return(
         <div className="login">
+            {console.log("--login page--", user)}
             <h1>Login</h1>
-            <input type="text" placeholder="Enter your Email"></input>
-            <input type="password" placeholder="Enter your Password"></input>
+            <input type="text" name="email" value={user.Email} placeholder="Enter your Email" onChange={handleChange}></input>
+            <input type="password" name="password" value={user.password} placeholder="Enter your Password" onChange={handleChange}></input>
             <div className="button">Login</div>
             <div>or</div>
             <div className="button">Register</div>
