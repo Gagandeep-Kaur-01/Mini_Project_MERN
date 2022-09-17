57b5c1de3b77cb97c03aea9db0f24813225cf11c
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-09-17 11:03:52 +0530

    route index file

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,6 +1,6 @@
 import books from "./books.js";
 import login from "./login.js";
-import register from "./register";
+import register from "./register.js";
 
 
 export default [books, login, register];
