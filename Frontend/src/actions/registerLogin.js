1f425639bb051d51deea3e3a82492e26764f60e7
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-09-17 14:33:53 +0530

    integrating login api

--- Frontend/src/actions/registerLogin.js
+++ Frontend/src/actions/registerLogin.js
@@ -14,3 +14,19 @@ export const registerNewUser = (payload) => {
     }
 }
 
+export const LoginUser = (payload) => {
+    return (dispatch) => {
+        axios
+        .post("http://localhost:5000/api/login/", payload)
+        .then(response => {
+            return response;
+    })
+    .catch((err) => {
+        console.log("----error-----", err)
+    })
+    }
+}
+
+
+
+
