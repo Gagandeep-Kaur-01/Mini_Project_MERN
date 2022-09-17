661996e11a35f71905b45ee2299ad7795c710e9a
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-09-17 13:55:01 +0530

    user registration api

--- Backend/routes/registerLogin.js
+++ Backend/routes/registerLogin.js
@@ -7,26 +7,35 @@ const app = express();
 
 app.post("/register", async(req, res) => {
     const { name, email, password } = req.body;
-    const user = new User({
-        name,
-        email,
-        password
-    })
-    // try {
-    //     const a1 =  await user.save() 
-    //     res.json(a1)
-    // }
-    // catch(err) {
-    //     res.send('Error')
-    // }
-    user.save(err => {
-        if(err) {
-            res.send("--error----", err)
+    // to check whether user already registered or not
+    User.findOne({
+        email: email
+    }, (err, user) => {
+        if(user) {
+            res.send({message: "User already registered"})
+        } else {
+            const user = new User({
+                name,
+                email,
+                password
+            })
+            // try {
+            //     const a1 =  await user.save() 
+            //     res.json(a1)
+            // }
+            // catch(err) {
+            //     res.send('Error')
+            // }
+            user.save(err => {
+                if(err) {
+                    res.send("--error----", err)
+                }
+                else {
+                    res.send({ message: "----Successfully registered----"})
+                }
+            })
         }
-        else {
-            res.send({ message: "----Successfully registered----"})
-        }
-    })
+    })    
 })
 
 app.post("/login", async(req, res) => {
