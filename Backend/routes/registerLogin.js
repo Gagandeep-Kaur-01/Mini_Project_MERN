74a44e21b256e5971fcba99a3be9fff792fc909c
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-09-17 13:35:34 +0530

    api for registering a user

--- Backend/routes/registerLogin.js
+++ Backend/routes/registerLogin.js
@@ -1,10 +1,32 @@
 import express from 'express';
 import models, { connectDb } from '../models/index.js';
 
+const User  = models.User;
+
 const app = express();
 
 app.post("/register", async(req, res) => {
-    res.send("--Register API---")    
+    const { name, email, password } = req.body;
+    const user = new User({
+        name,
+        email,
+        password
+    })
+    // try {
+    //     const a1 =  await user.save() 
+    //     res.json(a1)
+    // }
+    // catch(err) {
+    //     res.send('Error')
+    // }
+    user.save(err => {
+        if(err) {
+            res.send("--error----", err)
+        }
+        else {
+            res.send({ message: "----Successfully registered----"})
+        }
+    })
 })
 
 app.post("/login", async(req, res) => {
