cf6d3838333e5b047bc8320f5fc85f4d35a681d4
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-09-17 11:03:31 +0530

    created User schema

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
-
 import Book from './book.js';
+import User from './user.js'
 
 const connectDb = () => {
   return mongoose.connect('mongodb://localhost/libraryManagement');
 };
 
-const models = { Book };
+const models = { Book, User };
 
 export { connectDb };
 
