96fc548ade9dcddbc28c78851a44c37aa69fd10e
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-09-17 14:57:16 +0530

    login component

--- Frontend/src/Components/Login/login.js
+++ Frontend/src/Components/Login/login.js
@@ -1,7 +1,11 @@
 import React, {useState} from "react";
+import { useDispatch } from "react-redux";
+import * as actions from "../../actions"
 import "./login.css";
 
 const Login = () => {
+
+    const dispatch = useDispatch()
 
     const [ user, setUser] = useState({
         email: "abc@gmail.com",
@@ -16,15 +20,39 @@ const Login = () => {
         })
     }
 
+    const handleLogin = () => {
+        const { email, password } = user;
+        dispatch(actions.LoginUser(user))
+    }
+
     return(
         <div className="login">
             {console.log("--login page--", user)}
             <h1>Login</h1>
-            <input type="text" name="email" value={user.Email} placeholder="Enter your Email" onChange={handleChange}></input>
-            <input type="password" name="password" value={user.password} placeholder="Enter your Password" onChange={handleChange}></input>
-            <div className="button">Login</div>
-            <div>or</div>
-            <div className="button">Register</div>
+            <input 
+                type="text" 
+                name="email" 
+                value={user.email} 
+                placeholder="Enter your Email" 
+                onChange={handleChange}>                    
+            </input>
+            <input 
+                type="password" 
+                name="password" 
+                value={user.password} 
+                placeholder="Enter your Password" 
+                onChange={handleChange}>                    
+            </input>
+            <div className="button"
+                onClick = {handleLogin}>
+                Login
+            </div>
+            <div> 
+                or
+            </div>
+            <div className="button">
+                Register
+            </div>
         </div>
     )
 }
