1a95be70e8b310c35b78b49d51bd2e1d6520ca49
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-09-26 22:46:14 +0530

    toast messages

--- Frontend/src/actions/registerLogin.js
+++ Frontend/src/actions/registerLogin.js
@@ -1,5 +1,17 @@
 import axios from "axios";
-import { userConstant } from "../constants"
+import { notification, message } from "antd";
+import { userConstant } from "../constants";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+// const apiErrorNew = (err) => 
+// console.log("----err---", err)
+//     notification['error']({ 
+//         description: err.message || 'Something is wrong!' 
+//     });
+
+    const apiErrorNew = (err) => notification['error']({ description: err });
 
 
 export const registerNewUser = (payload, cb) => {
@@ -17,7 +29,7 @@ export const registerNewUser = (payload, cb) => {
     }
 }
 
-export const getLoginDetailSucess = (res) => ({
+export const getLoginDetailSuccess = (res) => ({
     type: userConstant.GET_LOGIN_DETAIL,
     response: res
 });
@@ -27,12 +39,20 @@ export const LoginUser = (payload, cb) => {
         axios
         .post("http://localhost:5000/api/login/", payload)
         .then(response => {
-            alert(response?.data?.message)
-            dispatch(getLoginDetailSucess(response?.data?.user))
+            console.log("--message----", response?.data?.message)
+            // notification['success']({
+            //     description: response?.data?.message || 'Success!',
+            //     onClick: () => {
+            //         console.log('Notification Clicked!');
+            //     },
+            // });
             cb()
+            dispatch(getLoginDetailSuccess(response?.data?.data))            
     })
     .catch((err) => {
-        console.log("----error-----", err)
+        console.log("----error=-----", err?.response?.data?.message);
+        toast(err?.response?.data?.message)
+        // apiErrorNew(err?.response?.data?.message);       
     })
     }
 }
